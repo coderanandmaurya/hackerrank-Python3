c168718651f879e913f40489aaeca1759070f0fe
Anand_Maurya <79273275+musketeer016@users.noreply.github.com>
2021-10-06 11:27:27 +0530

    week 1 : PlusMinus

---
+++ PlusMinus.py
@@ -0,0 +1,40 @@
+#1. Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+#soln:
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    x,y,z=0,0,0
+    
+    for i in range(0,len(arr)):
+        if arr[i]>0:
+            x=x+1
+            
+        elif arr[i]<0:
+            y=y+1
+            
+        else:
+            z=z+1
+    
+    print(x/len(arr))
+    print(y/len(arr))
+    print(z/len(arr))
+    
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    plusMinus(arr)
+
